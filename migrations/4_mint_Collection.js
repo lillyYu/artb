2f3da0232e06528aa6bcea8c5941a2ef1b602a26
klayhub-team <hello@klayhub.com>
2021-11-15 06:35:41 +0900

    ropsten deployed

--- migrations/4_mint_Collection.js
+++ migrations/4_mint_Collection.js
@@ -1,6 +1,8 @@
 const ArtbCollection = artifacts.require("ArtbCollection");
 const Address = {
   development: "0x55294a02d91F4DA8DeEb619692DeFe1005DA8A4A",
+  ropsten: "0x6F052Fc672D33061972ebe3CB0C509Fba13858fF",
+  ethereum: "0x6F052Fc672D33061972ebe3CB0C509Fba13858fF",
 };
 
 module.exports = async function (deployer, network) {
