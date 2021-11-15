fabcf3c9c77e3feb0e7f55a6956b0ce68834a3d3
klayhub-team <hello@klayhub.com>
2021-11-15 22:41:59 +0900

    deploy mainnet Mig(3)

--- migrations/4_mint_Collection.js
+++ migrations/4_mint_Collection.js
@@ -1,9 +1,4 @@
 const ArtbCollection = artifacts.require("ArtbCollection");
-const Address = {
-  development: "0x787FCeBEa97AB4e3B07C84713aF686eF4E50f0D1",
-  ropsten: "0x6F052Fc672D33061972ebe3CB0C509Fba13858fF",
-  ethereum: "0x6F052Fc672D33061972ebe3CB0C509Fba13858fF",
-};
 
 module.exports = async function (deployer, network, accounts) {
   const nft = await ArtbCollection.deployed();
