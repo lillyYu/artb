a47549c228fc6eb24a582c2df3d9e6dd84f78f3a
klayhub-team <hello@klayhub.com>
2021-11-15 19:02:26 +0900

    rinkeby deployed

--- migrations/4_mint_Collection.js
+++ migrations/4_mint_Collection.js
@@ -1,11 +1,11 @@
 const ArtbCollection = artifacts.require("ArtbCollection");
 const Address = {
-  development: "0x55294a02d91F4DA8DeEb619692DeFe1005DA8A4A",
+  development: "0x787FCeBEa97AB4e3B07C84713aF686eF4E50f0D1",
   ropsten: "0x6F052Fc672D33061972ebe3CB0C509Fba13858fF",
   ethereum: "0x6F052Fc672D33061972ebe3CB0C509Fba13858fF",
 };
 
-module.exports = async function (deployer, network) {
+module.exports = async function (deployer, network, accounts) {
   const nft = await ArtbCollection.deployed();
-  nft.mint(Address[network], 0, 100000, "0x0"); // 가을 축제
+  nft.mint(accounts[0], 0, 100000, "0x0"); // 가을 축제
 };
