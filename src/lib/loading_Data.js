0212aa9ad394fb4c3bb81b50840ccc6a34fe8475
kwonsujin <61954913+sjkwan92@users.noreply.github.com>
2021-11-15 15:34:07 +0900

    front fix mainpage

--- src/lib/loading_Data.js
+++ src/lib/loading_Data.js
@@ -3,6 +3,7 @@
 export const COPYRIGHT_DATA = [{
     name: "가을축제",
     writer: "남관",
+    term: "2021.09.26 12:00 ~ 2021.12.30 24:00",
     seller: "ArtB",
     price: "10,000",
     quantity: "123,123,123",
