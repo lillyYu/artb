62153abc0de886633cdcdb85baee2b7ead3d8093
toddheo <todd.heo@gmail.com>
2021-12-01 15:46:23 +0900

    merged postal code modal

--- src/Components/NftTrade/Popup/accountTransfer.js
+++ src/Components/NftTrade/Popup/accountTransfer.js
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import axios from "axios";
 import { useRecoilState } from "recoil";
 //store
 import { accountState } from "../../../store/web3";
+//daum postcode
+import DaumPostcode from "react-daum-postcode";
 
-function AccountTransferPopup({ setTransferPopup, amount, totalValue, address }) {
+function AccountTransferPopup({
+  setTransferPopup,
+  amount,
+  totalValue,
+  address,
+}) {
   const [account] = useRecoilState(accountState);
 
   const [name, setName] = useState("");
@@ -16,6 +23,7 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
   const [detailAddress2, setDetailAddress2] = useState("");
   const [middleNum, setMiddleNum] = useState("");
   const [lastNum, setLastNum] = useState("");
+  const [postcodePopup, setPostcodePopup] = useState(false);
 
   const postInfo = async () => {
     let data = {
@@ -34,6 +42,37 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
     console.log(totalValue);
   };
 
+  const postcodeModalStyle = {
+    position: "absolute",
+    top: "100px",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    zIndex: "100",
+    border: "1px solid #000000",
+    overflow: "hidden",
+  };
+  const postcodeHandleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    setPostAddress(data.zonecode);
+    setDetailAddress1(fullAddress);
+    setPostcodePopup(false);
+    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
+
   return (
     <Container className="Container">
       <Contents>
@@ -45,10 +84,14 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
           <img className="collectionImg" src="/collection1.png" />
           <div className="info">
             <div className="Text_Style_18" style={{ marginBottom: "8px" }}>
-              {address === "0x31B8696aa951771565EEcC9afBEB6F7eD87e2682" ? "남관" : "-"}
+              {address === "0x31B8696aa951771565EEcC9afBEB6F7eD87e2682"
+                ? "남관"
+                : "-"}
             </div>
             <div className="Text_Style_19" style={{ marginBottom: "23px" }}>
-              {address === "0x31B8696aa951771565EEcC9afBEB6F7eD87e2682" ? "가을축제" : "-"}
+              {address === "0x31B8696aa951771565EEcC9afBEB6F7eD87e2682"
+                ? "가을축제"
+                : "-"}
             </div>
             <div className="buyQuantity">
               <div className="Text_Style_20">{`신청 수량 ${amount} NFT`}</div>
@@ -69,11 +112,10 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
           사은품 수령을 위해 주소를 적어 주세요
         </div>
 
-        <div className="userInfo" >
+        <div className="userInfo">
           <div className="recipient">
             <div className="Text_Style_22">수령인*</div>
             <div>
-
               <input
                 className="nameInput"
                 placeholder="수령인 이름"
@@ -83,16 +125,22 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
             </div>
           </div>
           <div className="address">
-            <div className="Text_Style_22" >배송지*</div>
+            <div className="Text_Style_22">배송지*</div>
             <div style={{ display: "flex", gap: "0 8px", width: "433px" }}>
               <input
                 className="addressInput"
                 placeholder="우편번호"
                 value={postAddress}
                 onChange={(e) => setPostAddress(e.target.value)}
+                readOnly
               />
-              <div className="searchCode" style={{ cursor: "not-allowed" }}>
-                <div className="Text_Style_25" >우편번호 검색</div>
+              <div className="searchCode">
+                <div
+                  className="Text_Style_25"
+                  onClick={() => setPostcodePopup(!postcodePopup)}
+                >
+                  우편번호 검색
+                </div>
               </div>
             </div>
           </div>
@@ -102,6 +150,7 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
               placeholder="상세 주소 1"
               value={detailAddress1}
               onChange={(e) => setDetailAddress1(e.target.value)}
+              readOnly
             />
             <input
               className="detailInput"
@@ -112,7 +161,14 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
           </div>
           <div className="contact">
             <div className="Text_Style_22">연락처*</div>
-            <div style={{ display: "flex", gap: "0 15px", width: "433px", justifyContent: "space-between" }}>
+            <div
+              style={{
+                display: "flex",
+                gap: "0 15px",
+                width: "433px",
+                justifyContent: "space-between",
+              }}
+            >
               <input className="contactInput" />
               <input
                 className="contactInput"
@@ -127,29 +183,69 @@ function AccountTransferPopup({ setTransferPopup, amount, totalValue, address })
             </div>
           </div>
         </div>
-        {(name && postAddress && detailAddress1 && detailAddress2 && middleNum && lastNum) ?
-          <Link to={{
-            pathname: "/payment/coin",
-            state: {
-              totalValue: totalValue,
-              amount: amount,
-            }
-          }}>
-            <div className="button"
+        {name &&
+        postAddress &&
+        detailAddress1 &&
+        detailAddress2 &&
+        middleNum &&
+        lastNum ? (
+          <Link
+            to={{
+              pathname: "/payment/coin",
+              state: {
+                totalValue: totalValue,
+                amount: amount,
+              },
+            }}
+          >
+            <div
+              className="button"
               onClick={() => {
-                postInfo()
+                postInfo();
                 window.scrollTo(0, 0);
-              }}>
+              }}
+            >
               <div className="Text_Style_26">결제하기</div>
             </div>
           </Link>
-          : <div className="button"
-            onClick={() => { alert("필수 항목을 전부 입력해주시기 바랍니다.") }
-            }>
+        ) : (
+          <div
+            className="button"
+            onClick={() => {
+              alert("필수 항목을 전부 입력해주시기 바랍니다.");
+            }}
+          >
             <div className="Text_Style_26">결제하기</div>
           </div>
-        }
+        )}
       </Contents>
+
+      {postcodePopup ? (
+        <div>
+          <div
+            className="back"
+            onClick={() => setPostcodePopup(false)}
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              top: 0,
+              background: "white",
+              height: "80px",
+              padding: "20px 0 0 10px",
+              fontSize: "2rem",
+              cursor: "pointer",
+            }}
+          >
+            {"< 닫기"}
+          </div>
+          <DaumPostcode
+            onComplete={postcodeHandleComplete}
+            autoClose
+            style={postcodeModalStyle}
+          />
+        </div>
+      ) : null}
     </Container>
   );
 }
@@ -207,119 +303,119 @@ const Contents = styled.div`
     margin: 14px 14px 76px 0px;
     gap: 33px;
   }
-.userInfo{
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  gap: 20px;
-  .recipient {
-    display: flex;
-    margin-top: 70px;
-    justify-content: space-between;
-    align-items: center;
-    gap:20px;
-
-    .nameInput {
-      width: 400px;
-      height: 72px;
-      font-size: 24px;
-      padding-left: 27px;
-    }
-    input::-webkit-input-placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    input:-ms-input-placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    input::placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-  }
-  .address {
-    display: flex;
-    align-items: center;
-    gap:20px;
-    .addressInput {
-      width:48%;
-      height: 72px;
-      font-size: 24px;
-      padding-left: 27px;
-    }
-    input::-webkit-input-placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    input:-ms-input-placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    input::placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    .searchCode {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      width: 48%;
-      height: 74px;
-      background-color: #e64724;
-      border-radius: 4px;
-      cursor: pointer;
-    }
-  }
-  .detail {
+  .userInfo {
     display: flex;
     flex-direction: column;
     align-items: flex-end;
     gap: 20px;
-    width:400px;
-    .detailInput {
-      width: 100%;
-      height: 72px;
-      font-size: 24px;
-      padding-left: 27px;
-    }
-    input::-webkit-input-placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    input:-ms-input-placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-    input::placeholder {
-      font-weight: bold;
-      font-size: 24px;
-      color: #cccccccc;
-    }
-  }
-  .contact {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 60px;
-    gap:20px;
+    .recipient {
+      display: flex;
+      margin-top: 70px;
+      justify-content: space-between;
+      align-items: center;
+      gap: 20px;
 
-    .contactInput {
-      width:30%;
-      height: 72px;
-      font-size: 24px;
-      padding-left: 27px;
+      .nameInput {
+        width: 400px;
+        height: 72px;
+        font-size: 24px;
+        padding-left: 27px;
+      }
+      input::-webkit-input-placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      input:-ms-input-placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      input::placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+    }
+    .address {
+      display: flex;
+      align-items: center;
+      gap: 20px;
+      .addressInput {
+        width: 48%;
+        height: 72px;
+        font-size: 24px;
+        padding-left: 27px;
+      }
+      input::-webkit-input-placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      input:-ms-input-placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      input::placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      .searchCode {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 48%;
+        height: 74px;
+        background-color: #e64724;
+        border-radius: 4px;
+        cursor: pointer;
+      }
+    }
+    .detail {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      gap: 20px;
+      width: 400px;
+      .detailInput {
+        width: 100%;
+        height: 72px;
+        font-size: 24px;
+        padding-left: 27px;
+      }
+      input::-webkit-input-placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      input:-ms-input-placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+      input::placeholder {
+        font-weight: bold;
+        font-size: 24px;
+        color: #cccccccc;
+      }
+    }
+    .contact {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 60px;
+      gap: 20px;
+
+      .contactInput {
+        width: 30%;
+        height: 72px;
+        font-size: 24px;
+        padding-left: 27px;
+      }
     }
   }
-}
   .button {
     display: flex;
     height: 113px;
